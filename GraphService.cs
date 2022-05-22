81fe000aa56203e6966346b2c3ae05daf3079011
Jay Wang <jay.wang@hp.com>
2022-05-22 10:56:04 +0800

    feat: first commit

---
+++ GraphService.cs
@@ -0,0 +1,31 @@
+﻿using Neo4j.Driver;
+
+namespace neo4j_client;
+
+public class GraphService
+{
+    private IDriver _driver;
+
+    public GraphService(string uri, string user, string password)
+    {
+        _driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+    }
+
+    public async Task GetPersons()
+    {
+        using (var session = _driver.AsyncSession())
+        {
+            var result = await session.ReadTransactionAsync(async tx =>
+            {
+                var persons = await tx.RunAsync("match(p:Person) return p.name as name");
+                return (await persons.ToListAsync()).As<List<IRecord>>();
+            });
+            foreach (var person in result)
+            {
+                Console.WriteLine(person["name"]);
+            }
+        }
+        
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using neo4j_client;
+
+var graphService = new GraphService("bolt://localhost:7687", "neo4j", "Passw0rd");
+await graphService.GetPersons();
+Console.WriteLine("Press Enter to continue");
+Console.ReadLine();
